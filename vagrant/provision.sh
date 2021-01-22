7fc621b25958872bbc2ad081f310e76c0abe024f
Nikita Gusakov <gusnkt@gmail.com>
2021-01-22 14:19:47 +0300

    feat: upgrade docker & install kubectl

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -1,17 +1,37 @@
 set -e
 export DEBIAN_FRONTEND=noninteractive
 
-sudo apt update
-sudo apt upgrade -y
+sudo apt-get update
+sudo apt-get upgrade -y
 
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
-sudo apt update
+sudo apt-get -y remove docker docker-engine docker.io
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    gnupg2 \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
 
-sudo apt install -y nodejs npm yarn
-sudo apt install -y git
-sudo apt install -y docker.io docker-compose
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt-get update
+
+sudo apt-get install -y nodejs npm yarn
+sudo apt-get install -y git
+
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+sudo apt-get install -y kubectl
 
 sudo systemctl enable docker
 sudo gpasswd -a $USER docker

02128387b0076c2bfc67c81b1ed45c5e57d9c4d2
Eugene Formanenko <mo4islona@gmail.com>
2023-08-21 18:50:02 +0400

    Update provision.sh

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -2,12 +2,8 @@ set -e
 export DEBIAN_FRONTEND=noninteractive
 
 sudo apt-get update
-sudo apt-get upgrade -y
+# sudo apt-get upgrade -y
 
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-
-sudo apt-get -y remove docker docker-engine docker.io
 sudo apt-get install -y \
     apt-transport-https \
     ca-certificates \
@@ -15,15 +11,18 @@ sudo apt-get install -y \
     gnupg-agent \
     gnupg2 \
     software-properties-common
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/debian \
-   $(lsb_release -cs) \
-   stable"
 
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 sudo apt-get update
 
@@ -32,6 +31,8 @@ sudo apt-get install -y git jq
 
 sudo apt-get -y install docker-ce docker-ce-cli containerd.io
 sudo apt-get install -y kubectl
+
+npm install --global yarn
 
 sudo systemctl enable docker
 sudo gpasswd -a $USER docker
